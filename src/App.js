07ef3f1dbe7544915f0dcd9cac896ff85d1154a2
ReydelitoMatos <reydelitomatos@gmail.com>
2025-06-09 19:43:22 -0400

    Primer commit de ConfortBot

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>¡Vamos con todo, jefe! 🤖</h1>
+      <p>ConfortBot está listo para ayudarte.</p>
+    </div>
+  );
+}
+
+export default App;
